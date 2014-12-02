1fd26a8c49b66966e271df88bd1ee9baa94fa93a
Erik Knight <elk@uoregon.edu>
2014-12-02 09:34:17 -0800

    Initial Commit
    Still missing guts.

---
+++ PicRatings.m
@@ -0,0 +1,495 @@
+function PicRatings(varargin)
+% Rate all images, choose top X pics
+
+global wRect w XCENTER rects mids
+
+prompt={'SUBJECT ID'};
+defAns={'4444'};
+
+answer=inputdlg(prompt,'Please input subject info',1,defAns);
+
+ID=str2double(answer{1});
+
+%This and all tasks should just search for the specific images folder
+cd('/Users/canelab/Documents/StudyTasks/IMAGES_ORI_TASKS');
+
+PICS =struct;
+% if COND == 1;                   %Condtion = 1 is food. 
+    PICS.in.go = dir('good*.jpg');
+    PICS.in.no = dir('*bad*.jpg');
+%     PICS.in.neut = dir('*water*.jpg');    %Don't need ratings on neutral images
+% elseif COND == 2;               %Condition = 2 is not food (birds/flowers)
+%     PICS.in.go = dir('*bird*.jpg');
+%     PICS.in.no = dir('*flowers*.jpg');
+%     PICS.in.neut = dir('*mam*.jpg');
+% end
+
+piclist = [1:length(PICS.in.go) 1:length(PICS.in.no)];
+pictype = [ones(1,length(PICS.in.go)) zeros(1,length(PICS.in.no))];
+
+picpic = [piclist; pictype]';
+picpic = picpic(randperm(size(picpic,1)),:);
+
+ratings = zeros(length(picpic),2);
+
+[rects,mids] = DrawRectsGrid();
+
+for x = 1:length(picpic);
+    switch picpic
+        case {1}
+            tp = imfield(getfield(PICS,'in','go',{picpic(x,1)},'name'));
+        case {0}
+            tp = imfield(getfield(PICS,'in','no',{picpic(x,1)},'name'));
+    end
+    tpx = Screen('MakeTexture',w,tp);
+    
+    
+end
+
+
+
+
+
+end
+
+%%
+function [ rects,mids ] = DrawRectsGrid(varargin)
+%DrawRectGrid:  Builds a grid of squares with gaps in between.
+
+global wRect XCENTER
+
+%Size of image will depend on screen size. First, an area approximately 80%
+%of screen is determined. Then, images are 1/4th the side of that square
+%(minus the 3 x the gap between images.
+
+xlen = wRect(3)*.9;           %Make area covering about 90% of vertical dimension of screen.
+gap = 10;                       %Gap size between each image
+square_side = fix((xlen - 8*gap)/9); %Size of rect depends on size of screen.
+
+squart_x = XCENTER-(xlen/2);
+squart_y = wRect(4)*.8;         %Rects start @~80% down screen.
+
+rects = zeros(4,9);
+
+% for row = 1:DIMS.grid_row;
+    for col = 1:9;
+%         currr = ((row-1)*DIMS.grid_col)+col;
+        rects(1,col)= squart_x + (col-1)*(square_side+gap);
+        rects(2,col)= squart_y;
+        rects(3,col)= squart_x + (col-1)*(square_side+gap)+square_side;
+        rects(4,col)= squart_y + square_side;
+    end
+% end
+mids = [rects(1,:)+square_side/2; rects(2,:)+square_side/2];
+
+end
+
+function drawRatings(varargin)
+
+global w KEYS COLORS wRect rects mids
+
+colors=repmat(COLORS.WHITE',1,9);
+% rects=horzcat(allRects.rate1rect',allRects.rate2rect',allRects.rate3rect',allRects.rate4rect');
+
+%Needs to feed in "code" from KbCheck, to show which key was chosen.
+if nargin >= 1 && ~isempty(varargin{1})
+    response=varargin{1};
+    
+    key=find(response);
+    if length(key)>1
+        key=key(1);
+    end;
+    
+    switch key
+        
+        case {KEYS.ONE}
+            choice=1;
+        case {KEYS.TWO}
+            choice=2;
+        case {KEYS.THREE}
+            choice=3;
+        case {KEYS.FOUR}
+            choice=4;
+        case {KEYS.FIVE}
+            choice=5;
+        case {KEYS.SIX}
+            choice=6;
+        case {KEYS.SEVEN}
+            choice=7;
+        case {KEYS.EIGHT}
+            choice=8;
+        case {KEYS.NIVE}
+            choice=9;
+    end
+    
+    if exist('choice','var')
+        
+        
+        colors(:,choice)=COLORS.GREEN';
+        
+    end
+end
+
+if nargin>=2
+    
+    window=varargin{2};
+    
+else
+    
+    window=w;
+    
+end
+   
+
+Screen('TextFont', window, 'Arial');
+Screen('TextStyle', window, 1)
+oldSize = Screen('TextSize',window,45);
+
+% Screen('TextFont', w2, 'Arial');
+% Screen('TextStyle', w2, 1)
+% Screen('TextSize',w2,60);
+
+
+
+%draw all the squares
+Screen('FrameRect',window,colors,rects,1);
+
+
+% Screen('FrameRect',w2,colors,rects,1);
+
+
+%draw the text (1-4)
+for n = 1:9;
+    numnum = sprintf('%d',n);
+    CenterTextOnPoint(window,numnum,mids(1,n),mids(2,n),colors(:,1));
+end
+
+
+Screen('TextSize',window,oldSize);
+
+end
+
+
+%%
+function [nx, ny, textbounds] = CenterTextOnPoint(win, tstring, sx, sy,color)
+% [nx, ny, textbounds] = DrawFormattedText(win, tstring [, sx][, sy][, color][, wrapat][, flipHorizontal][, flipVertical][, vSpacing][, righttoleft])
+%
+% 
+
+numlines=1;
+
+if nargin < 1 || isempty(win)
+    error('CenterTextOnPoint: Windowhandle missing!');
+end
+
+if nargin < 2 || isempty(tstring)
+    % Empty text string -> Nothing to do.
+    return;
+end
+
+% Store data class of input string for later use in re-cast ops:
+stringclass = class(tstring);
+
+% Default x start position is left border of window:
+if isempty(sx)
+    sx=0;
+end
+
+% if ischar(sx) && strcmpi(sx, 'center')
+%     xcenter=1;
+%     sx=0;
+% else
+%     xcenter=0;
+% end
+
+xcenter=0;
+
+% No text wrapping by default:
+% if nargin < 6 || isempty(wrapat)
+    wrapat = 0;
+% end
+
+% No horizontal mirroring by default:
+% if nargin < 7 || isempty(flipHorizontal)
+    flipHorizontal = 0;
+% end
+
+% No vertical mirroring by default:
+% if nargin < 8 || isempty(flipVertical)
+    flipVertical = 0;
+% end
+
+% No vertical mirroring by default:
+% if nargin < 9 || isempty(vSpacing)
+    vSpacing = 1.5;
+% end
+
+% if nargin < 10 || isempty(righttoleft)
+    righttoleft = 0;
+% end
+
+% Convert all conventional linefeeds into C-style newlines:
+newlinepos = strfind(char(tstring), '\n');
+
+% If '\n' is already encoded as a char(10) as in Octave, then
+% there's no need for replacemet.
+if char(10) == '\n' %#ok<STCMP>
+   newlinepos = [];
+end
+
+% Need different encoding for repchar that matches class of input tstring:
+if isa(tstring, 'double')
+    repchar = 10;
+elseif isa(tstring, 'uint8')
+    repchar = uint8(10);    
+else
+    repchar = char(10);
+end
+
+while ~isempty(newlinepos)
+    % Replace first occurence of '\n' by ASCII or double code 10 aka 'repchar':
+    tstring = [ tstring(1:min(newlinepos)-1) repchar tstring(min(newlinepos)+2:end)];
+    % Search next occurence of linefeed (if any) in new expanded string:
+    newlinepos = strfind(char(tstring), '\n');
+end
+
+% % Text wrapping requested?
+% if wrapat > 0
+%     % Call WrapString to create a broken up version of the input string
+%     % that is wrapped around column 'wrapat'
+%     tstring = WrapString(tstring, wrapat);
+% end
+
+% Query textsize for implementation of linefeeds:
+theight = Screen('TextSize', win) * vSpacing;
+
+% Default y start position is top of window:
+if isempty(sy)
+    sy=0;
+end
+
+winRect = Screen('Rect', win);
+winHeight = RectHeight(winRect);
+
+% if ischar(sy) && strcmpi(sy, 'center')
+    % Compute vertical centering:
+    
+    % Compute height of text box:
+%     numlines = length(strfind(char(tstring), char(10))) + 1;
+    %bbox = SetRect(0,0,1,numlines * theight);
+    bbox = SetRect(0,0,1,theight);
+    
+    
+    textRect=CenterRectOnPoint(bbox,sx,sy);
+    % Center box in window:
+    [rect,dh,dv] = CenterRect(bbox, textRect);
+
+    % Initialize vertical start position sy with vertical offset of
+    % centered text box:
+    sy = dv;
+% end
+
+% Keep current text color if noone provided:
+if nargin < 5 || isempty(color)
+    color = [];
+end
+
+% Init cursor position:
+xp = sx;
+yp = sy;
+
+minx = inf;
+miny = inf;
+maxx = 0;
+maxy = 0;
+
+% Is the OpenGL userspace context for this 'windowPtr' active, as required?
+[previouswin, IsOpenGLRendering] = Screen('GetOpenGLDrawMode');
+
+% OpenGL rendering for this window active?
+if IsOpenGLRendering
+    % Yes. We need to disable OpenGL mode for that other window and
+    % switch to our window:
+    Screen('EndOpenGL', win);
+end
+
+% Disable culling/clipping if bounding box is requested as 3rd return
+% % argument, or if forcefully disabled. Unless clipping is forcefully
+% % enabled.
+% disableClip = (ptb_drawformattedtext_disableClipping ~= -1) && ...
+%               ((ptb_drawformattedtext_disableClipping > 0) || (nargout >= 3));
+% 
+
+disableClip=1;
+
+% Parse string, break it into substrings at line-feeds:
+while ~isempty(tstring)
+    % Find next substring to process:
+    crpositions = strfind(char(tstring), char(10));
+    if ~isempty(crpositions)
+        curstring = tstring(1:min(crpositions)-1);
+        tstring = tstring(min(crpositions)+1:end);
+        dolinefeed = 1;
+    else
+        curstring = tstring;
+        tstring =[];
+        dolinefeed = 0;
+    end
+
+    if IsOSX
+        % On OS/X, we enforce a line-break if the unwrapped/unbroken text
+        % would exceed 250 characters. The ATSU text renderer of OS/X can't
+        % handle more than 250 characters.
+        if size(curstring, 2) > 250
+            tstring = [curstring(251:end) tstring]; %#ok<AGROW>
+            curstring = curstring(1:250);
+            dolinefeed = 1;
+        end
+    end
+    
+    if IsWin
+        % On Windows, a single ampersand & is translated into a control
+        % character to enable underlined text. To avoid this and actually
+        % draw & symbols in text as & symbols in text, we need to store
+        % them as two && symbols. -> Replace all single & by &&.
+        if isa(curstring, 'char')
+            % Only works with char-acters, not doubles, so we can't do this
+            % when string is represented as double-encoded Unicode:
+            curstring = strrep(curstring, '&', '&&');
+        end
+    end
+    
+    % tstring contains the remainder of the input string to process in next
+    % iteration, curstring is the string we need to draw now.
+
+    % Perform crude clipping against upper and lower window borders for
+    % this text snippet. If it is clearly outside the window and would get
+    % clipped away by the renderer anyway, we can safe ourselves the
+    % trouble of processing it:
+    if disableClip || ((yp + theight >= 0) && (yp - theight <= winHeight))
+        % Inside crude clipping area. Need to draw.
+        noclip = 1;
+    else
+        % Skip this text line draw call, as it would be clipped away
+        % anyway.
+        noclip = 0;
+        dolinefeed = 1;
+    end
+    
+    % Any string to draw?
+    if ~isempty(curstring) && noclip
+        % Cast curstring back to the class of the original input string, to
+        % make sure special unicode encoding (e.g., double()'s) does not
+        % get lost for actual drawing:
+        curstring = cast(curstring, stringclass);
+        
+        % Need bounding box?
+%         if xcenter || flipHorizontal || flipVertical
+            % Compute text bounding box for this substring:
+            bbox=Screen('TextBounds', win, curstring, [], [], [], righttoleft);
+%         end
+        
+        % Horizontally centered output required?
+%         if xcenter
+            % Yes. Compute dh, dv position offsets to center it in the center of window.
+%             [rect,dh] = CenterRect(bbox, winRect);
+            [rect,dh] = CenterRect(bbox, textRect);
+            % Set drawing cursor to horizontal x offset:
+            xp = dh;
+%         end
+            
+%         if flipHorizontal || flipVertical
+%             textbox = OffsetRect(bbox, xp, yp);
+%             [xc, yc] = RectCenter(textbox);
+% 
+%             % Make a backup copy of the current transformation matrix for later
+%             % use/restoration of default state:
+%             Screen('glPushMatrix', win);
+% 
+%             % Translate origin into the geometric center of text:
+%             Screen('glTranslate', win, xc, yc, 0);
+% 
+%             % Apple a scaling transform which flips the direction of x-Axis,
+%             % thereby mirroring the drawn text horizontally:
+%             if flipVertical
+%                 Screen('glScale', win, 1, -1, 1);
+%             end
+%             
+%             if flipHorizontal
+%                 Screen('glScale', win, -1, 1, 1);
+%             end
+% 
+%             % We need to undo the translations...
+%             Screen('glTranslate', win, -xc, -yc, 0);
+%             [nx ny] = Screen('DrawText', win, curstring, xp, yp, color, [], [], righttoleft);
+%             Screen('glPopMatrix', win);
+%         else
+            [nx ny] = Screen('DrawText', win, curstring, xp, yp, color, [], [], righttoleft);
+%         end
+    else
+        % This is an empty substring (pure linefeed). Just update cursor
+        % position:
+        nx = xp;
+        ny = yp;
+    end
+
+    % Update bounding box:
+    minx = min([minx , xp, nx]);
+    maxx = max([maxx , xp, nx]);
+    miny = min([miny , yp, ny]);
+    maxy = max([maxy , yp, ny]);
+
+    % Linefeed to do?
+    if dolinefeed
+        % Update text drawing cursor to perform carriage return:
+        if xcenter==0
+            xp = sx;
+        end
+        yp = ny + theight;
+    else
+        % Keep drawing cursor where it is supposed to be:
+        xp = nx;
+        yp = ny;
+    end
+    % Done with substring, parse next substring.
+end
+
+% Add one line height:
+maxy = maxy + theight;
+
+% Create final bounding box:
+textbounds = SetRect(minx, miny, maxx, maxy);
+
+% Create new cursor position. The cursor is positioned to allow
+% to continue to print text directly after the drawn text.
+% Basically behaves like printf or fprintf formatting.
+nx = xp;
+ny = yp;
+
+% Our work is done. If a different window than our target window was
+% active, we'll switch back to that window and its state:
+if previouswin > 0
+    if previouswin ~= win
+        % Different window was active before our invocation:
+
+        % Was that window in 3D mode, i.e., OpenGL rendering for that window was active?
+        if IsOpenGLRendering
+            % Yes. We need to switch that window back into 3D OpenGL mode:
+            Screen('BeginOpenGL', previouswin);
+        else
+            % No. We just perform a dummy call that will switch back to that
+            % window:
+            Screen('GetWindowInfo', previouswin);
+        end
+    else
+        % Our window was active beforehand.
+        if IsOpenGLRendering
+            % Was in 3D mode. We need to switch back to 3D:
+            Screen('BeginOpenGL', previouswin);
+        end
+    end
+end
+
+return;
+end
+
